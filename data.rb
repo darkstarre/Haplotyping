6394b4c4873af5888635627af54dd7498e940953
Megan Nilles <darkstarre@hotmail.com>
2016-04-07 09:05:42 -0500

    build outline for reverse phylogeny search

--- data.rb
+++ data.rb
@@ -5,4 +5,12 @@
                       {"type" => "E1b1b",    "people" => "Etruscans"},
                       {"type" => "G2a",      "people" => "Etruscans"},
                       {"type" => "R1b-M269", "people" => "Etruscans"}]
+groupE1b = {"E1b" => {"E1b1b" => [{"E-M78" => "E-V13"},"E-M81"]}}
+
+groupG = {"G" => {"g2a" => "G2a3"}}
+
+groupIJ = {"IJ"=> [{"J"=>[{"J2" => "J2b"},"J1"]},{"i" => [{"pre-I1" => {"I1" => "I1a"}},{"I2" => [{"I2b" => {"I2b1"=> "I2b1a"}},{"I2a" => ["I2a1","I2a2"]}]}]}]}
+
+groupK = {"K" => ["T",{"NOP" => [{"P" => [{"R" => {"R1" =>[{"R1b" => {"R1b1" => {"R1b2" => ["R1b-S21",{"R1b-S116" => ["R1b-S28","R1b-L21"]}]}}},{"R1a" => {"R1a1" => "R1a1a"}}}]}, {"Q" => "Q1a"}]},{"NO" => {"N" => "N1c1"}}]}]}
+
 
